858c46e9df0f14c79e2a90ce87416cc4ea2970b8
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-07-30 17:03:25 +0200

    millstone 1

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App(){
 
 export default App;
 
-
+// Milestone 1: Recuperare e visualizzare i dati
 // Effettua una chiamata API a
 // http://localhost:3333/politicians
 
@@ -42,3 +42,10 @@ export default App;
 // Breve biografia (biography)
 
 // Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile.
+
+// Milestone 2: Implementare la ricerca ottimizzata
+// Aggiungi un campo di ricerca (<input type="text">) sopra la lista dei politici.
+// Permetti all’utente di filtrare i risultati in base a nome o biografia (se il testo cercato è incluso). Suggerimento: Creare un array derivato filtrato, che viene aggiornato solo quando cambia la lista di politici o il valore della ricerca.
+// Non usare useEffect per aggiornare l’array filtrato.
+
+// Obiettivo: Migliorare le prestazioni evitando ricalcoli inutili quando il valore della ricerca non cambia.
